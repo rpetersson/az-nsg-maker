0e03695cfdaaddfbde64e05f1fcd2bc670cae40d
robert petersson <rpetersson@gmail.com>
2025-02-02 20:59:57 +0700

    update

---
+++ asgRef.bicep
@@ -0,0 +1,29 @@
+// Create a Network Security Group (NSG)
+resource nsg 'Microsoft.Network/networkSecurityGroups@2021-02-01' = {
+  properties: {
+    securityRules: [
+      {
+        name: 'Allow-ASG-Inbound'
+        properties: {
+          priority: 100
+          direction: 'Inbound'
+          access: 'Allow'
+          protocol: 'Tcp'
+          sourcePortRange: '*'
+          destinationPortRange: '22'  // Change to required port (e.g., 443 for HTTPS)
+          sourceAddressPrefix: ''  // Not needed when using ASG
+          sourceApplicationSecurityGroups: [
+            {
+              id: asg.id
+            }
+          ]
+          destinationApplicationSecurityGroups: [
+            {
+              id: asg.id
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
---
+++ main.py
@@ -0,0 +1,40 @@
+from python_excel2json import parse_excel_to_json
+
+class GenerateNSGJson:
+    def __init__(self):
+        pass
+
+class LoadDataFromExcel:
+    def __init__(self, path: str):
+        path.self = path
+        
+    def readExcel(path: str):
+        excel_sheets_format = {
+            'start_row_sheet_parsing': 1,
+            'start_column_sheet_parsing': 0,
+            'sheet_formats': [
+                {
+                    'sheet_index': 0,
+                    'column_names': [
+                        {
+                            'name': 'Destination port',
+                            'type': 'str'
+                        },
+                        {
+                            'name': 'azASG',
+                            'type': 'str'
+                        },
+                        {
+                            'name': 'name3',
+                            'type': 'str'
+                        },
+                        {
+                            'name': 'name4',
+                            'type': 'float'
+                        }
+                    ],
+                    'is_ordered': True
+                }
+            ]
+}
+
