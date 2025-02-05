e80f2f807a77677d8510a15b28f3124c7246c688
robert petersson <rpetersson@gmail.com>
2025-02-05 14:27:16 +0700

    update

--- main.py
+++ main.py
@@ -1,58 +1,6 @@
 from python_excel2json import parse_excel_to_json
 import json
 
-class NSGRule:
-    def __init__(self, name: str, priority: int, direction: str, source_asg: str, destination_asg: str, destination_port: str) -> None:
-        self.name = name
-        self.priority = priority
-        self.direction = direction
-        self.source_asg = source_asg
-        self.destination_asg = destination_asg
-        self.destination_port = destination_port
-
-    def to_dict(self) -> dict:
-        return {
-            'name': self.name,
-            'priority': self.priority,
-            'direction': self.direction,
-            'access': 'Allow',
-            'protocol': 'Tcp',
-            'sourcePortRanges': ['*'],
-            'destinationPortRanges': [self.destination_port],
-            'sourceApplicationSecurityGroups': [{'id': self.source_asg}],
-            'destinationApplicationSecurityGroups': [{'id': self.destination_asg}]
-        }
-
-class GenerateNSGJson:
-    def __init__(self, data: list) -> None:
-        self.data = data
-        self.nsgs = []
-
-    def create_nsg_rule(self, item: dict, direction: str, count: int) -> NSGRule:
-        name = f"Allow-ASG-{direction}-{item.get('Destination port')}"
-        return NSGRule(
-            name=name,
-            priority=100 + count,
-            direction=direction,
-            source_asg=item.get('sourceAsg'),
-            destination_asg=item.get('destinationAsg'),
-            destination_port=item.get('Destination port')
-        )
-
-    def generateNsg(self) -> list:
-        for sheet in self.data:
-            results = sheet.get('results', [])
-            for count, item in enumerate(results, start=1):
-                outbound_nsg = self.create_nsg_rule(item, 'Outbound', count)
-                inbound_nsg = self.create_nsg_rule(item, 'Inbound', count)
-                self.nsgs.append(outbound_nsg.to_dict())
-                self.nsgs.append(inbound_nsg.to_dict())
-        return self.nsgs
-
-    def save_to_file(self, filename: str) -> None:
-        with open(filename, 'w') as f:
-            json.dump(self.nsgs, f, indent=2)
-
 class LoadDataFromExcel:
     def __init__(self, path: str) -> None:
         self.path = path
@@ -115,19 +63,72 @@ class LoadDataFromExcel:
         
     def getData(self) -> list:
         return self.data
+
+class NSGRule:
+    def __init__(self, priority: int, direction: str, source_asg: str, destination_asg: str, destination_port: str) -> None:
+        self.priority = priority
+        self.direction = direction
+        self.source_asg = source_asg
+        self.destination_asg = destination_asg
+        self.destination_port = destination_port
+
+    def to_dict(self) -> dict:
+        return {
+            'name': 'Allow-{}-to-{}-{}'.format(self.source_asg, self.destination_asg, self.destination_port),
+            'priority': self.priority,
+            'direction': self.direction,
+            'access': 'Allow',
+            'protocol': 'Tcp',
+            'sourcePortRanges': ['*'],
+            'destinationPortRanges': [self.destination_port],
+            'sourceApplicationSecurityGroups': [{'id': self.source_asg}],
+            'destinationApplicationSecurityGroups': [{'id': self.destination_asg}]
+        }
+
+
+
     
 # Create an instance of LoadDataFromExcel
 loader = LoadDataFromExcel('/Users/robban/Downloads/azmigratefiltered.xls')
 loader.loadExcel()
 data = loader.getData()  # Access the data variable
 
-print(json.dumps(data, indent=3))
+sheet_data = data[0].get("results")
+#sort sheet data by subnet name
+sortedSheetData = sorted(sheet_data, key=lambda x: x['Az snet'])
 
-# Create an instance of GenerateNSGJson
-nsg_generator = GenerateNSGJson(data)
-nsgs = nsg_generator.generateNsg()
+c = 0
+previousSubnet = ""
 
-print(json.dumps(nsgs, indent=2))
+for i in sortedSheetData:
+    subnet = i.get("Az snet")
+    if previousSubnet != subnet:
+        c = 0
+    c += 1
+    previousSubnet = subnet
 
-# Save the NSGs to a file
-nsg_generator.save_to_file('nsgs.json')
+    inboundNsgRule = NSGRule(
+        priority=1000+c, 
+        direction='Inbound', 
+        source_asg=i.get("sourceAsg"), 
+        destination_asg=i.get("destinationAsg"), 
+        destination_port=i.get("Destination port")).to_dict()
+    
+    outobundNsgRule = NSGRule(
+        priority=1000+c, 
+        direction='Outbound', 
+        source_asg=i.get("destinationAsg"), 
+        destination_asg=i.get("sourceAsg"), 
+        destination_port=i.get("Destination port")).to_dict()
+
+    print(json.dumps(inboundNsgRule, indent=4))
+
+    print(json.dumps(outobundNsgRule, indent=4))
+
+    with open(f'{subnet}_inbound.json', 'a') as f:
+        f.write(json.dumps(inboundNsgRule, indent=4))
+        f.write('\n')
+    
+    with open(f'{subnet}_outbound.json', 'a') as f:
+        f.write(json.dumps(outobundNsgRule, indent=4))
+        f.write('\n')
